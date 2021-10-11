a8b3f79a975afe972fd299b93ccd6c3a68ef2a91
mysliwy112 <261731@student.pwr.edu.pl>
2021-10-11 20:27:13 +0200

    Adds newClass method

--- src/main/java/git/test/App.java
+++ src/main/java/git/test/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        newClass();        
+    }
+    
+    public static void newClass() {
+    	System.out.println( "New World!" );
     }
 }
