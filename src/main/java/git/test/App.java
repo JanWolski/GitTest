116c14b14d50c2a3df01b4117191b12c5ce476f6
mysliwy112 <261731@student.pwr.edu.pl>
2021-10-17 15:06:17 +0200

    Changes newClass function name to fajnyClass function name

--- src/main/java/git/test/App.java
+++ src/main/java/git/test/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        newClass();        
+        fajnyClass();
     }
     
-    public static void newClass() {
+    public static void fajnyClass() {
     	System.out.println( "New World!" );
     }
 }
