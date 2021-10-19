971b1365a0a7b1446f855c786419d86d93355c96
mysliwy112 <261731@student.pwr.edu.pl>
2021-10-19 09:36:34 +0200

    Changed function name

--- src/main/java/git/test/App.java
+++ src/main/java/git/test/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        testFunction();
+        test2Function();
     }
     
-    public static void testFunction() {
+    public static void test2Function() {
     	System.out.println( "New World!" );
     }
 }
